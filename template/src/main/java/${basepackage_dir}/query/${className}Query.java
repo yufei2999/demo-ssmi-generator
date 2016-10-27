84da4e040ca44343fac10061bc29693e5753fb98
temp <yuye2999@126.com>
2016-10-27 13:17:50 +0800

    optimize

--- template/src/main/java/${basepackage_dir}/query/${className}Query.java
+++ template/src/main/java/${basepackage_dir}/query/${className}Query.java
@@ -4,6 +4,7 @@ package ${basepackage}.query;
 
 import com.yufei.base.BaseQuery;
 import ${basepackage}.model.${className};
+import java.util.Calendar;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
@@ -21,21 +22,46 @@ public class ${className}Query extends BaseQuery<${className}> {
 }
 
 <#macro generateFields>
-	<#list table.columns as column>
-	/**
-	 * ${column.columnAlias}
-	 */
+<#list table.columns as column>
+	/** ${column.columnAlias} */
+	<#if column.isDateTimeColumn && !column.contains("begin,start,end")>
+	private ${column.javaType} ${column.columnNameLower}Begin;
+	private ${column.javaType} ${column.columnNameLower}End;
+	<#else>
 	private ${column.javaType} ${column.columnNameLower};
-	</#list>
+	</#if>
+</#list>
 </#macro>
 
 <#macro generateProperties>
-	<#list table.columns as column>
+<#list table.columns as column>
+	<#if column.isDateTimeColumn && !column.contains("begin,start,end")>
+	public ${column.javaType} get${column.columnName}Begin() {
+		return this.${column.columnNameLower}Begin;
+	}
+	public void set${column.columnName}Begin(${column.javaType} value) {
+		this.${column.columnNameLower}Begin = value;
+	}
+	public ${column.javaType} get${column.columnName}End() {
+		return this.${column.columnNameLower}End;
+	}
+	public void set${column.columnName}End(${column.javaType} value) {
+		if(value != null){
+			Calendar calendar = Calendar.getInstance();
+			calendar.setTime(value);
+			calendar.add(Calendar.DAY_OF_MONTH, 1);
+			this.${column.columnNameLower}End = calendar.getTime();
+		}else {
+			this.${column.columnNameLower}End = value;
+		}
+	}
+	<#else>
 	public ${column.javaType} get${column.columnName}() {
 		return this.${column.columnNameLower};
 	}
 	public void set${column.columnName}(${column.javaType} value) {
 		this.${column.columnNameLower} = value;
 	}
-	</#list>
+	</#if>
+</#list>
 </#macro>
